928c5e3aac059181178445e1e044277068b540a1
Vitor Almeida <kassielalmeida@gmail.com>
2023-11-11 00:07:59 -0300

    feat: remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UploadModule } from './upload/upload.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), UploadModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
